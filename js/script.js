8cc594d9a668b76aa5a5acd00875ad66dae4bafd
Marion <marion.vagnerdamicourt@kpmg.co.uk>
2019-10-26 11:53:09 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,158 +1,143 @@
-// A javascript-enhanced crossword puzzle [c] Jesse Weisbeck, MIT/GPL 
+// A javascript-enhanced crossword puzzle
 (function($) {
 	$(function() {
 		// provide crossword entries in an array of objects like the following example
-		// Position refers to the numerical order of an entry. Each position can have 
+		// Position refers to the numerical order of an entry. Each position can have
 		// two entries: an across entry and a down entry
 		var puzzleData = [
 			 	{
-					clue: "First letter of greek alphabet",
-					answer: "alpha",
+					clue: "Jiayu pretends to come from this city",
+					answer: "shanghai",
 					position: 1,
 					orientation: "across",
-					startx: 1,
+					startx: 3,
 					starty: 1
 				},
 			 	{
-					clue: "Not a one ___ motor, but a three ___ motor",
-					answer: "phase",
-					position: 3,
+					clue: "Hina will celebrate this festival this month",
+					answer: "diwali",
+					position: 2,
 					orientation: "across",
-					startx: 7,
-					starty: 1
+					startx: 3,
+					starty: 2
 				},
 				{
-					clue: "Created from a separation of charge",
-					answer: "capacitance",
+					clue: "Ewelina left us to take on that job",
+					answer: "consultant",
+					position: 3,
+					orientation: "across",
+					startx: 2,
+					starty: 3
+				},
+				{
+					clue: "He is Rishab's favourite superhero",
+					answer: "batman",
+					position: 4,
+					orientation: "across",
+					startx: 3,
+					starty: 4
+				},
+				{
+					clue: "Hina can read our mind because she studied this",
+					answer: "psychology",
 					position: 5,
 					orientation: "across",
 					startx: 1,
-					starty: 3
+					starty: 5
 				},
 				{
-					clue: "The speeds of engines without and accelaration",
-					answer: "idlespeeds",
+					clue: "Ewelina got two lapdances during this show",
+					answer: "magicmike",
+					position: 6,
+					orientation: "across",
+					startx: 2,
+					starty: 6
+				},
+				{
+					clue: "Ewelina grow these creatures in her garden",
+					answer: "snails",
+					position: 7,
+					orientation: "across",
+					startx: 5,
+					starty: 7
+				},
+				{
+					clue: "Dylan's leaving do",
+					answer: "karaoke",
 					position: 8,
 					orientation: "across",
-					startx: 1,
-					starty: 5
-				},
-				{
-					clue: "Complex resistances",
-					answer: "impedances",
-					position: 10,
-					orientation: "across",	
-					startx: 2,
-					starty: 7
-				},
-				{
-					clue: "This device is used to step-up, step-down, and/or isolate",
-					answer: "transformer",
-					position: 13,
-					orientation: "across",
-					startx: 1,
-					starty: 9
-				},
-				{
-					clue: "Type of ray emitted frm the sun",
-					answer: "gamma",
-					position: 16,
-					orientation: "across",
-					startx: 1,
-					starty: 11
-				},
-				{
-					clue: "C programming language operator",
-					answer: "cysan",
-					position: 17,
-					orientation: "across",
-					startx: 7,
-					starty: 11
-				},
-				{
-					clue: "Defines the alpha-numeric characters that are typically associated with text used in programming",
-					answer: "ascii",
-					position: 1,
-					orientation: "down",
-					startx: 1,
-					starty: 1
-				},
-				{
-					clue: "Generally, if you go over 1kV per cm this happens",
-					answer: "arc",
-					position: 2,
-					orientation: "down",
-					startx: 5,
-					starty: 1
-				},
-				{
-					clue: "Control system strategy that tries to replicate the human through process (abbr.)",
-					answer: "ann",
-					position: 4,
-					orientation: "down",
-					startx: 9,
-					starty: 1
-				},
-				{
-					clue: "Greek variable that usually describes rotor positon",
-					answer: "theta",
-					position: 6,
-					orientation: "down",
-					startx: 7,
-					starty: 3
-				},
-				{
-					clue: "Electromagnetic (abbr.)",
-					answer: "em",
-					position: 7,
-					orientation: "down",
-					startx: 11,
-					starty: 3
-				},
-				{
-					clue: "No. 13 across does this to a voltage",
-					answer: "steps",
-					position: 9,
-					orientation: "down",
-					startx: 5,
-					starty: 5
-				},
-				{
-					clue: "Emits a lout wailing sound",
-					answer: "siren",
-					position: 11,
-					orientation: "down",
-					startx: 11,
-					starty: 7
-				},
-				{
-					clue: "Information technology (abbr.)",
-					answer: "it",
-					position: 12,
-					orientation: "down",
-					startx: 1,
+					startx: 3,
 					starty: 8
 				},
 				{
-					clue: "Asynchronous transfer mode (abbr.)",
-					answer: "atm",
-					position: 14,
-					orientation: "down",
-					startx: 3,
+					clue: "The name of mount on the island Sophie is from",
+					answer: "pelee",
+					position: 9,
+					orientation: "across",
+					startx: 6,
 					starty: 9
 				},
 				{
-					clue: "Offset current control (abbr.)",
-					answer: "occ",
+					clue: "Dylan got a weird accent, in which country did he grow up?",
+					answer: "Australia",
+					position: 10,
+					orientation: "across",
+					startx: 2,
+					starty: 10
+				},
+				{
+					clue: "We got stuck in a police van with him and tried to escape",
+					answer: "raven",
+					position: 11,
+					orientation: "across",
+					startx: 3,
+					starty: 11
+				},
+				{
+					clue: "Marion is currently learning this",
+					answer: "coding",
+					position: 12,
+					orientation: "across",
+					startx: 1,
+					starty: 12
+				},
+				{
+					clue: "Marion beats people up in this martial art",
+					answer: "kendo",
+					position: 13,
+					orientation: "across",
+					startx: 4,
+					starty: 13
+				},
+				{
+					clue: "Wild animal we can see in the office",
+					answer: "meerkat",
+					position: 14,
+					orientation: "across",
+					startx: 1,
+					starty: 14
+				},
+				{
+					clue: "Something in Sophie's living room which annoys her no end",
+					answer: "cabinet",
 					position: 15,
-					orientation: "down",
-					startx: 7,
-					starty: 9
-				}
-			] 
-	
+					orientation: "across",
+					startx: 2,
+					starty: 15
+				},
+				{
+					clue: "What is Sophie's partner profession?",
+					answer: "optician",
+					position: 16,
+					orientation: "across",
+					startx: 4,
+					starty: 16
+				},
+
+			]
+
 		$('#puzzle-wrapper').crossword(puzzleData);
-		
+
 	})
-	
+
 })(jQuery)
